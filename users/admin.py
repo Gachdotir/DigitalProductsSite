df97d89df858b9c1fe8f50f4c697f89f07af33f9
Aria Gachkar <Ariagachkar78@gmail.com>
2025-01-16 08:07:46 +0330

    add phone number to user model

--- users/admin.py
+++ users/admin.py
@@ -1,25 +1,28 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(BaseUserAdmin):
     model = User
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_active')
-    list_filter = ('is_staff', 'is_active')
+    list_display = ('username', 'email', 'phone_number', 'is_active', 'is_staff')
+    list_filter = ('is_active', 'is_staff')
+    search_fields = ('username', 'email', 'phone_number')
+    ordering = ('date_joined',)
+
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal Info', {'fields': ('first_name', 'last_name')}),
-        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('email', 'phone_number', 'first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'is_staff', 'is_active')}),
+            'fields': ('username', 'email', 'phone_number', 'password1', 'password2'),
+        }),
     )
-    search_fields = ('email',)
-    ordering = ('email',)
 
 
 admin.site.register(User, UserAdmin)
